e8eaef411a42ca0a1a46b33dbc9a987e70706be5
Ras-svet <kulagina.cveta@yandex.ru>
2023-11-23 02:01:02 +0300

    fix: тесты

--- app.js
+++ app.js
@@ -46,7 +46,6 @@ app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
-	console.log(req.user._id)
   res.status(statusCode).send({
     message: statusCode === '500'
       ? 'Произошла ошибка на сервере'
